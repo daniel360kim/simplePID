3dedd49485f67f61b14e1544b581ab08b8adb4fc
NM Rocketry Reviews <70825614+nmrocketryreviews@users.noreply.github.com>
2022-01-15 17:32:44 -0700

    Create PID.cpp

---
+++ src/PID.cpp
@@ -0,0 +1,30 @@
+#include "PID.h"
+
+PID::PID(double kp, double ki, double kd) { //setting the PID gains
+    this->kp = kp;
+    this->ki = ki;
+    this->kd = kd;
+}
+
+void PID::setOffset(double offset){ //offsets results. useful for shifting values
+    this->offset = offset;
+}
+
+void PID::setRatio(double ratio){ //value multiplier (gear ratio etc.)
+    this->ratio = ratio;
+}
+
+double PID::updateSetpoint(double setpoint){
+    this->sp = setpoint;
+    return sp;
+}
+double PID::updatePID(double input, double dt){
+    double error = input - sp;
+
+    _p = kp * error; //Proportion
+    _i = ki * (_i + error * dt); //Integral
+    _d = kd * ((error - pre_error) / dt);
+
+    return ratio * (_p + _i + _d) + offset;
+}
+
